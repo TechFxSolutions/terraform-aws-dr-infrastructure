ae2df28163c47480a27ccaf9bac2b491d7597c34
TechFxSolutions <contact@techfxsolutions.co.uk>
2025-12-10 10:35:02 +0000

    feat: add security module outputs

---
+++ modules/security/outputs.tf
@@ -0,0 +1,36 @@
+# Security Module Outputs
+
+output "alb_security_group_id" {
+  description = "ID of the ALB security group"
+  value       = aws_security_group.alb.id
+}
+
+output "web_security_group_id" {
+  description = "ID of the web tier security group"
+  value       = aws_security_group.web.id
+}
+
+output "app_security_group_id" {
+  description = "ID of the application tier security group"
+  value       = aws_security_group.app.id
+}
+
+output "database_security_group_id" {
+  description = "ID of the database tier security group"
+  value       = aws_security_group.database.id
+}
+
+output "bastion_security_group_id" {
+  description = "ID of the bastion host security group"
+  value       = aws_security_group.bastion.id
+}
+
+output "public_nacl_id" {
+  description = "ID of the public network ACL"
+  value       = aws_network_acl.public.id
+}
+
+output "private_nacl_id" {
+  description = "ID of the private network ACL"
+  value       = aws_network_acl.private.id
+}
